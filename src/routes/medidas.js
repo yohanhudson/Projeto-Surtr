80b668406dd0f374019767d34d0e304d6310b42a
yohanhudson <yohan.hudson@sptech.school>
2022-05-30 16:15:05 -0300

    apiLike fazer

--- src/routes/medidas.js
+++ src/routes/medidas.js
@@ -11,6 +11,8 @@ router.get("/tempo-real/:idAquario", function (req, res) {
     medidaController.buscarMedidasEmTempoReal(req, res);
 });
 
+router.get("numeroCurtida/:idUsuario", medidaController.BuscarNumeroCurtidas);
+
 
 
 module.exports = router;
