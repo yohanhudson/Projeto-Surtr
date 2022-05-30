07c0e0b6768913ca859736e2936e7872eaca02c2
yohanhudson <yohan.hudson@sptech.school>
2022-05-30 05:39:39 -0300

    fazendo o like

--- src/database/config.js
+++ src/database/config.js
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    user: "aluno",
+    user: "root",
     database: "Surtr",
-    password: "sptech",
+    password: "3.1415926535yan",
 };
 
 function executar(instrucao) {
